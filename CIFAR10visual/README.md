1b7f287927d8b67f191e6ddf81b265b4676b98ab
Vladislav Tananaev <v.d.tananaev@gmail.com>
2016-10-24 19:03:25 +0200

    Update, cleanup.

---
+++ CIFAR10visual/README.md
@@ -0,0 +1,20 @@
+Dataset
+=======
+To download CIFAR10 dataset use:
+
+  ./get_datasets.sh
+
+It will upload end extract files in CIFAR10 folder.
+
+Building
+========
+
+    mkdir build && cd build
+    cmake ..
+    make
+
+Running
+=======
+
+    ./CIFAR10visual
+
--- CIFAR10visual/visualizer.h
+++ CIFAR10visual/visualizer.h
@@ -9,17 +9,19 @@
 #define VISUALIZER_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QDir>
 #include <stdio.h>      /* printf, fgets */
 #include <stdlib.h>     /* atof */
 #include <fstream>
 #include <iostream>
 #include <vector>
 #include "ui_visualizer.h"
-/*
+
 namespace Ui {
 class visualizer;
 }
-*/
+
 class visualizer : public QMainWindow, private Ui_visualizer
 {
     Q_OBJECT
@@ -30,15 +32,16 @@ public:
     explicit visualizer(QWidget *parent = 0);
     ~visualizer();
     void init();
-    bool readCFAR(const char* filename);
+    bool readCFAR(const char* dirname);
 
-private:    
-    ///Ui::visualizer *ui;
+private slots:
+    void on_actionLoad_from_folder_triggered();
+
+private:
+    Ui::visualizer *ui;
     std::vector<int> labels;
     std::vector< std::vector<int> > images; 
     std::vector<std::string> categories;
-
-
 
 };
 
