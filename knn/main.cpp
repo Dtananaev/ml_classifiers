55e9ee46e2fc9034a2bb3776576b58bf98eb54b2
Denis Tananaev <d.d.tananaev@gmail.com>
2017-05-02 14:26:31 +0200

    knn mainfix

--- knn/main.cpp
+++ knn/main.cpp
@@ -6,9 +6,13 @@ int main(int argc, char *argv[])
     knn w;
    if(w.trainSetread("../CIFAR10") && w.testSetread("../CIFAR10")){
         w.init();
+        w.show();
+    }else{
+       std::cerr<<"Can't open CIFAR10 dataset from ../CIFAR10 folder"<<"\n";
+       return 0;
     }
 
-   w.show();
+
     
-    return a.exec();
+
 }
